6544e6108311e2d5d480621981b37e791539cfe9
revyrob <kayle.robson@gmail.com>
2023-03-27 18:03:05 -0700

    added libraries

--- server.js
+++ server.js
@@ -1,30 +1,30 @@
-const express = require('express')
-const mysql = require('mysql')
+const express = require("express");
+const mysql = require("mysql");
 
-require('dotenv').config()
+require("dotenv").config();
 
 //starting express app
-const app = express()
+const app = express();
 
 //connecting to sql database
 const connection = mysql.createConnection({
-    host        : process.env.DB_HOST,
-    user        : process.env.DB_USER,
-    password    : process.env.DB_PASS,
-})
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+});
 
 if (connection) {
-    console.log('OK!')
+  console.log("OK!");
 }
 
 //routes
-app.get('/', (req,res) => {
-    res.send("database stuff")
-})
+app.get("/", (req, res) => {
+  res.send("database stuff");
+});
 
 app.listen(8080, () => {
-    console.log('listening')
-})
+  console.log("listening");
+});
 
 // querying for all planets
 /* connection.query('SELECT * FROM solar_system_schema.planet', 
@@ -36,7 +36,7 @@ app.listen(8080, () => {
     }
 ) */
 
-console.log('\n')
+console.log("\n");
 
 // querying for specific planet
 /* connection.query('SELECT * FROM solar_system_schema.planet WHERE planet_id = "pl-pl"', 
@@ -48,7 +48,7 @@ console.log('\n')
     }
 ) */
 
-console.log('\n')
+console.log("\n");
 
 // querying for a specific planet and its moons
 /* connection.query('SELECT * FROM solar_system_schema.planet, solar_system_schema.moon WHERE solar_system_schema.planet.planet_id = solar_system_schema.moon.planet_id AND solar_system_schema.planet.planet_id = "pl-ea"', 
@@ -60,4 +60,4 @@ console.log('\n')
     }
 ) */
 
-connection.end()
+connection.end();
