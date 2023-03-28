57e559e2a9d429f715b0ed69de5a63f20ffbee8e
Madison <madxb98@gmail.com>
2023-03-27 21:24:12 -0500

    set up router

---
+++ controller/routesController.js
@@ -0,0 +1,53 @@
+const express = require('express')
+const mysql = require('mysql')
+
+// Connect to database
+const pool = mysql.createPool({
+    host        : process.env.DB_HOST,
+    user        : process.env.DB_USER,
+    password    : process.env.DB_PASS,
+})
+
+// Views all planets
+exports.home = (req, res) => {
+    // Connect to DB
+    pool.getConnection((err, connection) => {
+        if (err) {
+            throw err // didnt connect
+        }
+        console.log('connected to DB')
+        // Using connection
+        connection.query('SELECT * FROM solar_system_schema.planet', (err,rows) => {
+            //release connection
+            connection.release()
+            if (!err) {
+                res.send({rows})
+            } else {
+                console.log(err)
+            }
+            console.log('data: \n', rows)
+        })
+    })
+}
+
+// Views specific planet
+exports.planet = (req, res) => {
+    // Connect to DB
+    pool.getConnection((err, connection) => {
+        if (err) {
+            throw err // didnt connect
+        } 
+        console.log('connected to DB')
+        // Using connection
+        connection.query('SELECT * FROM solar_system_schema.planet, solar_system_schema.moon WHERE solar_system_schema.planet.planet_id = solar_system_schema.moon.planet_id AND solar_system_schema.planet.planet_id = ?',[req.params.planet_id], (err,rows) => {
+            //release connection
+            connection.release()
+            if (!err) {
+                res.send({rows})
+            } else {
+                console.log(err)
+            }
+            console.log('data: \n', rows)
+        })
+    })
+}
--- server.js
+++ server.js
@@ -9,58 +9,11 @@ const app = express()
 
 app.use(cors());
 
-//connecting to sql database
-const connection = mysql.createConnection({
-    host        : process.env.DB_HOST,
-    user        : process.env.DB_USER,
-    password    : process.env.DB_PASS,
-})
-
-if (connection) {
-    console.log('OK!')
-}
-
-//routes
-app.get('/', (req,res) => {
-    res.send("database stuff")
-})
+// Routing
+const routes = require('./routes/routes')
+app.use('/', routes)
 
 app.listen(8080, () => {
     console.log('listening')
 })
 
-// querying for all planets
-/* connection.query('SELECT * FROM solar_system_schema.planet', 
-    function(err, res, fields) {
-        if (err) {
-            console.log(err)
-        }
-        console.log(res)
-    }
-) */
-
-console.log('\n')
-
-// querying for specific planet
-/* connection.query('SELECT * FROM solar_system_schema.planet WHERE planet_id = "pl-pl"', 
-    function(err, res, fields) {
-        if (err) {
-            console.log(err)
-        }
-        console.log(res)
-    }
-) */
-
-console.log('\n')
-
-// querying for a specific planet and its moons
-/* connection.query('SELECT * FROM solar_system_schema.planet, solar_system_schema.moon WHERE solar_system_schema.planet.planet_id = solar_system_schema.moon.planet_id AND solar_system_schema.planet.planet_id = "pl-ea"', 
-    function(err, res, fields) {
-        if (err) {
-            console.log(err)
-        }
-        console.log(res)
-    }
-) */
-
-connection.end()
