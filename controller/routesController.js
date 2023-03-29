eb370947845aa3938f307cb320ef16a9ddbf46b7
Madison <madxb98@gmail.com>
2023-03-29 16:24:25 -0500

    Fixed a querying issue for planets without moons

--- controller/routesController.js
+++ controller/routesController.js
@@ -40,13 +40,25 @@ exports.planet = (req, res) => {
         console.log('connected to DB')
         // Using connection
         connection.query('SELECT * FROM solar_system_schema.planet, solar_system_schema.moon WHERE solar_system_schema.planet.planet_id = solar_system_schema.moon.planet_id AND solar_system_schema.planet.planet_id = ?',[req.params.planet_id], (err,rows) => {
-            //release connection
-            connection.release()
             if (!err) {
-                res.send({rows})
+                // If the planet has no moons, we do a second query for the planet itself
+                if(Object.keys(rows).length < 1) {
+                    connection.query('SELECT * FROM solar_system_schema.planet WHERE solar_system_schema.planet.planet_id = ?', [req.params.planet_id], (err, rows) => {
+                        if (!err) {
+                            res.send({rows})
+                        } else {
+                            console.log(err)
+                        }
+                    })
+                } else {
+                    console.log(`HERE IS THE DATA: ${rows}`)
+                    res.send({rows})
+                }
             } else {
                 console.log(err)
             }
+            //release connection
+            connection.release()
             console.log('data: \n', rows)
         })
     })
